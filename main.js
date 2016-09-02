7978a6d7d0747eaa1c901ab85814663243a3736a
rdgd <ryan.is.gray@gmail.com>
2016-09-02 16:28:05 -0500

    Now able to take command line argument for files to compare.

--- main.js
+++ main.js
@@ -1,48 +1,55 @@
-/*
-  1. Take glob input for files.
-  2. ASCII art, 1337h4x02
-  3. Read config file
-*/
+#!/usr/bin/env node
 
 var fs = require('fs');
 var chalk = require('chalk');
 var towelie = require('./towelie');
-console.log(towelie);
-
-var docPaths = ['/docs/doc1.txt', '/docs/doc2.txt', '/docs/doc3.txt', '/docs/doc4.txt'];
+var cli = require('commander');
+var glob = require('glob');
+var path = require('path');
+var docPath = process.argv[2].toString();
+var docPaths;
 var docs = [];
 var messages = [];
-
-docPaths.forEach(function (docPath, i) {
-  fs.readFile(__dirname + docPath, function (err, data) {
-    if (!err) { docs.push({ content: data.toString(), filePath: docPath, pi: i }); }
-    if (docs.length === docPaths.length) { compareDocs(docs); }
+// Reading in all documents and only beginning the comparison once all have been read into memory
+glob(path.join(process.cwd(), docPath), function (err, paths){
+  docPaths = paths;
+  docPaths.forEach(function (docPath, i) {
+    fs.readFile(docPath, function (err, data) {
+      console.log(docPath);
+      if (!err) { docs.push({ content: data.toString(), filePath: docPath, pi: i }); }
+      if (docs.length === docPaths.length) { compareDocs(docs); }
+    });
   });
 });
 
+console.log(chalk.green(towelie));
+
 function compareDocs (docs) {
+  console.log(docPaths);
+  // i represents the "root document"
   for (var i = 0; i < docs.length; i++) {
     var iPOriginal = removeEmpty(docs[i].content.split('\n\n'));
     var iP = normalize(iPOriginal);
-    
+    // x represents the "comparison document"
     for (var x = 0; x < docs.length; x++) {
       var xPOriginal = removeEmpty(docs[x].content.split('\n\n'));
       var xP = normalize(xPOriginal);
 
       if (i === x) { continue; }
-      // First let's check for total equality
+      // First let's check for total equality. If equal, then no reason to compare at a deeper level.
       if (docs[i].content === docs[x].content) {
-        messages.push(`Docs ${chalk.blue(docs[i].filePath)} and ${chalk.blue(docs[x].filePath)} are ${chalk.red('identical')}!`);
-        //docPaths = docPaths.filter(function (value, ind) { return ind !== docs[i].pi && ind !== docs[x].pi; });
-        //docs = docs.filter(function (v, ind) { return ind !== i && ind !== x; });
+        messages.push(`Docs ${chalk.yellow(docs[i].filePath)} and ${chalk.yellow(docs[x].filePath)} are ${chalk.red('identical')}! ${chalk.bgYellow(chalk.blue('W') + chalk.cyan('T') + chalk.green('F'))} ${chalk.bgRed('!!!')}`);
         continue;
       }
 
-      // Check for paragraph-level equality
+      /*
+        Check for paragraph-level equality by iterating over the "root document" paragraphs (y), 
+        and for each paragraph iterating over the current "comparison document" paragraphs (z)
+      */
       for (let y = 0; y < iP.length; y++) {
         for (let z = 0; z < xP.length; z++) {
           if (iP[y] === xP[z]) {
-            messages.push(`Docs ${chalk.blue(docs[i].filePath)} and ${chalk.blue(docs[x].filePath)} repeat the following: \n\n\t ${chalk.red(iPOriginal[y])}`);
+            messages.push(`Docs ${chalk.yellow(docs[i].filePath)} and ${chalk.yellow(docs[x].filePath)} repeat the following: \n\n\t ${chalk.red(iPOriginal[y])} \n`);
           }
         }
       }
@@ -62,4 +69,6 @@ function removeEmpty (arr) {
 
 function report (messages) {
   messages.forEach(function (msg) { console.log(msg); });
+  chalk.green(`Towelie says, don't forget your towel when you get out of the pool`);
+  chalk.red(`Towelie found ${messages.length} violations!`);
 }

c79637ebf36f2328fa26b9904b5f276215157f16
Ryan Gray <ryan.is.gray@gmail.com>
2016-09-08 18:54:32 -0500

    Now if a file detects a duplicate file that is a duplicate of another file, we will see that the three files are the same all in one message, instead of several separate messages. Also added a number of comments to the code so I do not forget what I did in the future.

--- index.js
+++ index.js
@@ -16,10 +16,17 @@ var towelie = require('./assets/towelie');
 init();
 
 function init () {
+  // We show towelie picture for fun
   console.log(chalk.green(towelie));
+  // We expect the glob argument to ALWAYS be the first argument 
   let glob = process.argv[2];
   if(!glob) { glob = '**/*.*'; }
-  // The procedure is to (1) read (2) compare the contents and (3) report towlie's findings
+
+  /*
+    This application has 4 different stages: (1) configure (2) read (3) compare the contents
+    and (4) report towlie's findings. In stage 2, read, we pass in the global variable "config", required above, 
+    otherwise we are just piping functions
+  */
   configure()
     .then(function (config) { return read(glob.toString(), config); })
     .then(function (docs){ return compare(docs); })
@@ -29,18 +36,18 @@ function init () {
 
 function configure () {
   return new Promise(function (resolve, reject) {
+    // Attempt to read the .trc file, which is the designated name for a twly config file
     fs.readFile(process.cwd() + '/.trc', 'utf-8', function (err, data) {
-      let o = {
-        ignore: []
-      };
+      let o = { ignore: [] };
       if (err) {
         o.ignore = config.ignore;
       } else {
+        // The required format of the config file is JSON
         let userConf = JSON.parse(data);
-        if (userConf.ignore) {
-          let ignore = userConf.ignore;
-          ignore.forEach(function (p) { o.ignore.push(path.join(process.cwd(), p)); });
-        }
+        let ignore = userConf.ignore;
+        // If user supplied ignore values, we get their fully qualified paths and add them to ignore array
+        ignore && ignore.forEach(function (p) { o.ignore.push(path.join(process.cwd(), p)); });
+        // Checking for the existence of individual properties and copying over their values if they exist
         if (userConf.failureThreshold) { config.failureThreshold = userConf.failureThreshold; }
         if (userConf.minLines) { config.minLines = userConf.minLines; }
         if (userConf.minChars) { config.minChars = userConf.minChars; }
@@ -51,11 +58,15 @@ function configure () {
 }
 
 function read (pathsToRead, config) {
-  // Reading in all documents and only beginning the comparison once all have been read into memory
   return new Promise(function (resolve, reject) {
     var docs = [];
-    glob(path.join(process.cwd(), pathsToRead), config, function (err, paths){
+    glob(path.join(process.cwd(), pathsToRead), config, function (err, paths) {
       paths.forEach(function (p, i) {
+
+        /*
+          Reading in all documents and only firing off the comparison once all have been read.
+          This is signaled by invoking the promise's resolve function and passing it an array of documents. 
+        */
         fs.readFile(p, function (err, data) {
           if (err) { throw err; }
           state.totalFiles++;
@@ -78,21 +89,34 @@ function compare (docs) {
     let iP = normalize(iPOriginal);
     let hash = hashString(minify(docs[i].content));
 
+    /*
+      We check if the hash of ALL of the minified content in current document already exists in our array of hashes
+      If it does, that means we have a duplicate of an entire document, so we check to see if there is a message with 
+      that hash as a reference, and if there is then we add the docpath to the message... otherwise just add message
+    */
     if (hash in fullDocHashes) {
+      let existingMsgInd = fullDocHashes[hash].msgInd;
+      if (existingMsgInd) {
+        messages[existingMsgInd].docs.push(docs[i].filePath);
+      } else {
+        // Sort of clever: before augmenting the length of the array by pushing to it, I am grabbing the current length for that index
+        fullDocHashes[hash].msgInd = messages.length;
+        messages.push(new Message([docs[i].filePath, docs[fullDocHashes[hash].ind].filePath], 0, ''));
+      }
+      // Increment the relevant counters for reporting
       state.dupedLines += (numLines(docs[i].content) * 2);
       state.numFileDupes++;
-      messages.push(new Message([docs[i].filePath, docs[fullDocHashes[hash]].filePath], 0, ''));
       continue;
     }
 
-    fullDocHashes[hash] = i;
+    fullDocHashes[hash] = { ind: i };
 
     for (let p = 0; p < iP.length; p++) {
       if (!isBigEnough(iPOriginal[p], (config.minLines - 1), config.minChars)) { continue; }
       let pHash = hashString(iP[p]);
       if (pHash in allBlockHashes) {
         let file1 = docs[i].filePath;
-        let file2 = docs[fullDocHashes[allBlockHashes[pHash]]].filePath;
+        let file2 = docs[fullDocHashes[allBlockHashes[pHash]].ind].filePath;
         state.dupedLines += (numLines(iPOriginal[p]) * 2);
         state.numParagraphDupes++;
         if (file1 === file2) {
@@ -108,7 +132,10 @@ function compare (docs) {
       }
     }
   }
-
+  /*
+    We just return a value here instead of resolving a promise, because we are not in a promise and do not
+    need one because the above operations are synchronous
+  */
   return messages;
 }
 

4a145cd63e871a3909e7e82fbd88c1a8912db669
rdgd <ryan.is.gray@gmail.com>
2016-09-16 17:00:46 -0500

    Fixed critical issue which was mistakenly treating directories as files and as a result, causing errors.

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ var path = require('path');
 
 module.exports = {
   ignore: [ path.join(process.cwd(),'node_modules/**/*.*'), path.join(process.cwd(),'bower_components/**/*.*'), path.join(process.cwd(),'.git/**/*.*') ],
+  nodir: true,
   failureThreshold: 95,
   minLines: 4,
   minChars: 100

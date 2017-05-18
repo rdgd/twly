a4b8140905caf471e6c81924bfe0dc02e5377895
rdgd <ryan.is.gray@gmail.com>
2017-05-17 22:45:59 -0500

    Broke out some matching into functions.

--- index.js
+++ index.js
@@ -71,115 +71,108 @@ function read (pathsToRead, config) {
   });
 }
 
-// Break this into smaller functions... it's a bit unwieldy.
 function compare (docs) {
   let messages = [];
   let fullDocHashes = new Map();
   let allBlockHashes = new Map(); 
 
   for (let i = 0; i < docs.length; i++) {
-    let blocks = makeBlockArray(docs[i].content);
-    let minifiedBlocks = minifyBlocks(blocks);
     let docHash = hashString(minify(docs[i].content));
-    let fullDocumentMatch = fullDocHashes.has(docHash);
-
     /*
       We check if the hash of ALL of the minified content in current document already exists in our array of hashes
       If it does, that means we have a duplicate of an entire document, so we check to see if there is a message with
       that hash as a reference, and if there is then we add the docpath to the message... otherwise just add message
     */
-    if (!fullDocumentMatch) {
+    let fullDocMatched = fullDocHashes.has(docHash);
+    if (!fullDocMatched) {
       fullDocHashes.set(docHash, { docInd: i });
     } else {
-      let existingMsgInd = fullDocHashes.get(docHash).msgInd;
-      let previouslyMatched = existingMsgInd > -1;
-      if (previouslyMatched) {
-        let msg = messages[existingMsgInd];
-        (msg.docs.indexOf(docs[i].filePath) === -1) && msg.docs.push(docs[i].filePath);
-      } else {
-        // msgInd is a way to point to a "message" related to a hash, which is faster than iterating over all messages looking for a hash
-        fullDocHashes.get(docHash).msgInd = messages.length;
-        messages.push(new Message([docs[i].filePath, docs[fullDocHashes.get(docHash).docInd].filePath], constants.IDENTICAL_FILE, docHash));
-      }
-      // Increment the relevant counters for reporting
+      makeDocMatchMessage(i, docHash, fullDocHashes, docs, messages);
       state.dupedLines += numLines(docs[i].content);
       state.numFileDupes++;
-      /*
-        If we don't continue here, then we will start matching the blocks of files which are pure duplicates
-        However, if we do continue, then if a different file shares a fragment with the current file, we will not realize.
-        The solution might be to not continue here, but skip blocks who have hashes that map files which are perfect duplicates,
-        so check below at match time... a duplicate message will have already been created. Related to: https://github.com/rdgd/twly/issues/4
-      */
       continue;
     }
-
     // If the file being examined is not a text file, we want to evaluate only it's full signature
     if (!isTextFile(docs[i].filePath)) { continue; }
+    /*
+      If we don't continue here when fullDocMatched, then we will start matching the blocks of files which are pure duplicates
+      However, if we do continue, then if a different file shares a fragment with the current file, we will not realize.
+      The solution might be to not continue here, but skip blocks who have hashes that map files which are perfect duplicates,
+      so check below at match time... a duplicate message will have already been created. Related to: https://github.com/rdgd/twly/issues/4
+    */
 
+    let blocks = makeBlockArray(docs[i].content);
+    let minifiedBlocks = minifyBlocks(blocks);
     // We iterate over the current document's minified blocks
     for (let b = 0; b < minifiedBlocks.length; b++) {
-      // First we must check if this block is even worth checking, as we have config params which set some criteria for the content size
       if (!meetsSizeCriteria(blocks[b], (config.minLines - 1), config.minChars)) { continue; }
-      let block = blocks[b];
+      // First we must check if this block is even worth checking, as we have config params which set some criteria for the content size
       let blockHash = hashString(minifiedBlocks[b]);
       let blockMatch = allBlockHashes.has(blockHash);
-      /*
-        Checking if minified block hash exists in array of all block hashes. If it doesn't
-        then we just add the hash to the global block/block hash array. If it does then we need to know
-        if it has simply been added there or also has a message associated with it.
-      */
       if (!blockMatch) {
-        /*
-          Assigning the value to the document hash because we want to be able to look up the correct index 
-          for the doc in the docs array and to get that index we look at the full document hash index 
-          object with the document hash as its key
-        */
         allBlockHashes.set(blockHash, { docInd: fullDocHashes.get(docHash).docInd }); 
-      }
-
-      if (blockMatch) {
-        let docInd = allBlockHashes.get(blockHash).docInd;
-        let file1 = docs[i].filePath;  // Current file of main file loop
-        let file2 = docs[docInd].filePath; // File where match was found
-        let inSameFile = file1 === file2;
-        let dupeMsgInd = findDuplicateMsgInd(blockHash, messages);
-        let firstTimeMatched = dupeMsgInd === -1;
-
-        state.dupedLines += numLines(block);
-        state.numBlockDupes++;
-
-        if (inSameFile) {
-          // TODO: Add count for number of times repeated in the same file
-          state.numBlockDupesInFile++;
-          messages.push(new Message([file1], constants.INTRA_FILE_DUPLICATE, blockHash, blocks[b]));
-        }
-
-        if (!inSameFile && !firstTimeMatched) {
-          // This is also an 'inter-file duplicate' scenario
-          let duplicateMsg = messages[dupeMsgInd];
-          let alreadyReportedByCurrentFile = duplicateMsg.docs.indexOf(file1) > -1;
-          if (!alreadyReportedByCurrentFile) { duplicateMsg.docs.push(file1); }
-        }
-
-        if (!inSameFile && firstTimeMatched) {
-          /*
-            Need to figure out if there is a message with the same files for a message we are about to write,
-            and if so, add the content to that message. TODO: We also need to be able to add that content's hash to an array
-            of hashes instead of just a single hash so that we can pick up duplicate content still.
-          */
-          let dupeMsgInd = getMsgIndByFiles([file1, file2], messages);
-          if (dupeMsgInd === -1) {
-            messages.push(new Message([file1, file2], constants.INTER_FILE_DUPLICATE, blockHash, blocks[b]));
-          } else {
-            messages[dupeMsgInd].content.push(blocks[b]);
-            messages[dupeMsgInd].hashes.push(blockHash);
-          }
-        }
+      } else {
+        makeBlockMatchMessage(i, b, docs, blocks, minifiedBlocks, allBlockHashes, fullDocHashes, docHash, messages);
       }
     }
   }
 
   return messages;
+}
+
+function makeBlockMatchMessage (i, b, docs, blocks, minifiedBlocks, allBlockHashes, fullDocHashes, docHash, messages) {
+  let block = blocks[b];
+  let blockHash = hashString(minifiedBlocks[b]);
+  let docInd = allBlockHashes.get(blockHash).docInd;
+  let file1 = docs[i].filePath;  // Current file of main file loop
+  let file2 = docs[docInd].filePath; // File where match was found
+  let inSameFile = file1 === file2;
+  let dupeMsgInd = findDuplicateMsgInd(blockHash, messages);
+  let firstTimeMatched = dupeMsgInd === -1;
+
+  state.dupedLines += numLines(block);
+  state.numBlockDupes++;
+
+  if (inSameFile) {
+    // TODO: Add count for number of times repeated in the same file
+    state.numBlockDupesInFile++;
+    messages.push(new Message([file1], constants.INTRA_FILE_DUPLICATE, blockHash, block));
+  }
+
+  if (!inSameFile && !firstTimeMatched) {
+    // This is also an 'inter-file duplicate' scenario
+    let duplicateMsg = messages[dupeMsgInd];
+    let alreadyReportedByCurrentFile = duplicateMsg.docs.indexOf(file1) > -1;
+    if (!alreadyReportedByCurrentFile) { duplicateMsg.docs.push(file1); }
+  }
+
+  if (!inSameFile && firstTimeMatched) {
+    /*
+      Need to figure out if there is a message with the same files for a message we are about to write,
+      and if so, add the content to that message. TODO: We also need to be able to add that content's hash to an array
+      of hashes instead of just a single hash so that we can pick up duplicate content still.
+    */
+    let dupeMsgInd = getMsgIndByFiles([file1, file2], messages);
+    if (dupeMsgInd === -1) {
+      messages.push(new Message([file1, file2], constants.INTER_FILE_DUPLICATE, blockHash, block));
+    } else {
+      messages[dupeMsgInd].content.push(block);
+      messages[dupeMsgInd].hashes.push(blockHash);
+    }
+  }
+}
+
+function makeDocMatchMessage (i, docHash, fullDocHashes, docs, messages) {
+  let existingMsgInd = fullDocHashes.get(docHash).msgInd;
+  let previouslyMatched = existingMsgInd > -1;
+  if (previouslyMatched) {
+    let msg = messages[existingMsgInd];
+    (msg.docs.indexOf(docs[i].filePath) === -1) && msg.docs.push(docs[i].filePath);
+  } else {
+    // msgInd is a way to point to a "message" related to a hash, which is faster than iterating over all messages looking for a hash
+    fullDocHashes.get(docHash).msgInd = messages.length;
+    messages.push(new Message([docs[i].filePath, docs[fullDocHashes.get(docHash).docInd].filePath], constants.IDENTICAL_FILE, docHash));
+  }
 }
 
 function report (messages) {
